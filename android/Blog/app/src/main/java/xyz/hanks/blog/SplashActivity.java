2372d6f6ad12ee6afb4be28577877d48340e43d3
hanks-zyh <zhangyuhan2014@gmail.com>
2016-01-23 19:39:56 +0800

    add SplashActivity

---
+++ android/Blog/app/src/main/java/xyz/hanks/blog/SplashActivity.java
@@ -0,0 +1,16 @@
+package xyz.hanks.blog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by hanks on 16/1/23.
+ */
+public class SplashActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
