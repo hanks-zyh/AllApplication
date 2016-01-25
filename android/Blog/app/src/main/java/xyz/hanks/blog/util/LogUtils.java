17530380cb44098f9b5fb78c47e9ec0dd7db5f5e
hanks-zyh <zhangyuhan2014@gmail.com>
2016-01-25 22:52:41 +0800

    add logger & add LogUtils method

--- android/Blog/app/src/main/java/xyz/hanks/blog/util/LogUtils.java
+++ android/Blog/app/src/main/java/xyz/hanks/blog/util/LogUtils.java
@@ -1,7 +1,58 @@
 package xyz.hanks.blog.util;
 
+import com.orhanobut.logger.Logger;
+
+import xyz.hanks.blog.BuildConfig;
+
 /**
+ * log管理
  * Created by hanks on 16/1/25.
  */
 public class LogUtils {
+    private static final String TAG = "zyhLog";
+    public static boolean showLog = BuildConfig.SHOW_LOG; // 是否需要打印bug，可以在application的onCreate函数里面初始化
+
+    /**
+     * 打印catch的异常信息
+     * @param e
+     */
+    public static void logE(Exception e) {
+        if (showLog) {
+            e.printStackTrace();
+        }
+    }
+
+    // 下面四个是默认tag的函数
+    public static void i(String msg) {
+        if (showLog) Logger.i(msg);
+    }
+
+    public static void d(String msg) {
+        if (showLog) Logger.d(msg);
+    }
+
+    public static void e(String msg) {
+        if (showLog) Logger.e(msg);
+    }
+
+    public static void v(String msg) {
+        if (showLog) Logger.v(msg);
+    }
+
+    // 下面是传入自定义tag的函数
+    public static void i(String tag, String msg) {
+        if (showLog) Logger.i(tag,msg);
+    }
+
+    public static void d(String tag, String msg) {
+        if (showLog) Logger.i(tag,msg);
+    }
+
+    public static void e(String tag, String msg) {
+        if (showLog) Logger.i(tag,msg);
+    }
+
+    public static void v(String tag, String msg) {
+        if (showLog) Logger.i(tag,msg);
+    }
 }
