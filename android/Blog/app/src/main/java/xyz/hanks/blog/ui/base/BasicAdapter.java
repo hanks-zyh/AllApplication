f749f59ef41ed790e67cb09f533932a2c2347138
hanks-zyh <zhangyuhan2014@gmail.com>
2016-02-02 21:58:07 +0800

    add base class

---
+++ android/Blog/app/src/main/java/xyz/hanks/blog/ui/base/BasicAdapter.java
@@ -0,0 +1,7 @@
+package xyz.hanks.blog.ui.base;
+
+/**
+ * Created by hanks on 16/2/2.
+ */
+public class BasicAdapter {
+}
---
+++ android/Blog/app/src/main/java/xyz/hanks/blog/ui/widget/AvatarImageView.java
@@ -0,0 +1,7 @@
+package xyz.hanks.blog.ui.widget;
+
+/**
+ * Created by hanks on 16/2/2.
+ */
+public class AvatarImageView {
+}
