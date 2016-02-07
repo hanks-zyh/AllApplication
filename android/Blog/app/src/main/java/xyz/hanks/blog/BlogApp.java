b5b7a0980c870fe08b771c3e0e7d91d23777a7ab
hanks-zyh <zhangyuhan2014@gmail.com>
2016-02-07 21:57:53 +0800

    add custom font file

--- android/Blog/app/src/main/java/xyz/hanks/blog/BlogApp.java
+++ android/Blog/app/src/main/java/xyz/hanks/blog/BlogApp.java
@@ -4,18 +4,26 @@ import android.app.Application;
 import android.content.Context;
 import android.os.StrictMode;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+
 /**
+ * Application类
  * Created by hanks on 16/1/25.
  */
 public class BlogApp extends Application {
 
     private static BlogApp sInstance;
 
+    public static Context getInstance() {
+        return sInstance;
+    }
+
     @Override
     public void onCreate() {
         super.onCreate();
         sInstance = this;
 
+        //严格模式
         StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
                 .detectCustomSlowCalls()// API等级11，使用StrictMode.noteSlowCode
                 .detectDiskReads()
@@ -24,9 +32,13 @@ public class BlogApp extends Application {
                 .penaltyLog()
                 .penaltyFlashScreen()// API等级11
                 .build());
+
+        //自定义字体
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/AvenirLTStd-Medium.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
     }
 
-    public static Context getInstance() {
-        return sInstance;
-    }
 }
--- android/Blog/app/src/main/java/xyz/hanks/blog/ui/base/BaseActivity.java
+++ android/Blog/app/src/main/java/xyz/hanks/blog/ui/base/BaseActivity.java
@@ -1,15 +1,20 @@
 package xyz.hanks.blog.ui.base;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
 
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
+
 /**
  * Created by hanks on 16/2/2.
  */
 public class BaseActivity extends AppCompatActivity {
 
-
+    @Override protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 }
