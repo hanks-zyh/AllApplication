c514ee1263bf05a9308c2c7aecc8b0d42a1b26f2
hanks-zyh <zhangyuhan2014@gmail.com>
2016-02-09 17:27:04 +0800

    add recyclerview

--- android/Blog/app/src/main/java/xyz/hanks/blog/ui/base/BasicAdapter.java
+++ android/Blog/app/src/main/java/xyz/hanks/blog/ui/base/BasicAdapter.java
@@ -1,7 +1,23 @@
 package xyz.hanks.blog.ui.base;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
 /**
+ * 封装RecyclerView的Adapter
  * Created by hanks on 16/2/2.
  */
-public class BasicAdapter {
+public class BasicAdapter extends RecyclerView.Adapter {
+
+    @Override public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override public int getItemCount() {
+        return 0;
+    }
 }
--- android/Blog/app/src/main/java/xyz/hanks/blog/ui/main/MainActivity.java
+++ android/Blog/app/src/main/java/xyz/hanks/blog/ui/main/MainActivity.java
@@ -1,15 +1,25 @@
 package xyz.hanks.blog.ui.main;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 import xyz.hanks.blog.R;
 import xyz.hanks.blog.ui.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Bind(R.id.rv) RecyclerView mRecyclerView;
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @Override protected void onDestroy() {
+        ButterKnife.unbind(this);
+        super.onDestroy();
     }
 }
